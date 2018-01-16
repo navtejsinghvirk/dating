9afb4e64a1467114a305aba6d5fc089d1fba2575
navtej singh <nsingh20@mail.greenriver.edu>
2018-01-15 20:57:55 -0500

    dating Files

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+            //require the autolaod file
+            require_once ('vendor/autoload.php');
+
+            //create an instance of the base class
+            $f3 = Base::instance();
+
+            //Set debug level
+            $f3->set('DEBUG',3);
+
+            //define a default route
+            $f3->route('GET /',
+                function() {
+                    $view = new View;
+                    echo $view->rendor
+                    ('dating/home.html');
+                }
+            );
+
+            //run fat free
+            $f3->run();
+?>
